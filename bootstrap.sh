e0295b3cd37b090d8cc9afeef978816b615f12c8
cnDenis <cyanodenis@gmail.com>
2026-05-04 14:09:24 +0800

    1. 清除旧的文件

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-pip install -r requirements.txt
-
-cd ..
-git clone https://github.com/getpelican/pelican-plugins.git
-git clone git@github.com:cnDenis/pelican-themes.git
-cd pelican-themes
-git checkout denis
